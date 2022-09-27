3502e660a8175ccebde79a8505070ad500c3dfa0
frar20ab1 <frar20ab@student.cbs.dk>
2022-09-27 14:07:27 +0200

    1.

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtRBk1CNFOD9aZUC5mArHdDc0b68uaJ3o",
+  authDomain: "godkendelsesopgave1-a1c57.firebaseapp.com",
+  projectId: "godkendelsesopgave1-a1c57",
+  storageBucket: "godkendelsesopgave1-a1c57.appspot.com",
+  messagingSenderId: "118450841898",
+  appId: "1:118450841898:web:1ecddbcd3b21477e48d653"
+};
+
+
 export default function App() {
   return (
     <View style={styles.container}>
